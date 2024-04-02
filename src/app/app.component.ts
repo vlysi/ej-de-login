4da9c95f1eb4377f0ca911546fe0bc4b3809b0dd
Vale L <106398144+vlysi@users.noreply.github.com>
2024-04-02 15:41:36 -0300

    Add source code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUserCircle, faBars, faLock} from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ej-de-login';
+  faUserCircle = faUserCircle;
+  faBars = faBars;
+  faLock = faLock;
 }
